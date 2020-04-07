7c19f52cf28f42f4b5751eeb375936c8a334fef4
Nicole <chelseanbr@gmail.com>
2020-04-07 11:48:52 -0700

    finalized cleaning company names for now

--- src/data_cleaning_eda.py
+++ src/data_cleaning_eda.py
@@ -2,5 +2,9 @@ def clean_cat_data(df, cols):
     result_df = df.copy()
     for col in cols:
         col_str = 'Cleaned_' + col
-        result_df[col_str] = result_df[col].str.lower().str.strip()
+        result_df[col_str] = result_df[col].str.lower()
+        result_df[col_str] = result_df[col_str].str.strip()
+        result_df[col_str] = result_df[col_str].str.replace(',', '')
+        result_df[col_str] = result_df[col_str].str.replace('.', '')
+    result_df = result_df.drop(cols, axis=1)
     return result_df
