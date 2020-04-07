81862de2ab56f35cf3745e97e11d143dacff4378
Nicole <chelseanbr@gmail.com>
2020-04-07 13:50:33 -0700

    added function to get groupby counts df, added first plot image - chemical counts bar chart

--- src/data_cleaning_eda.py
+++ src/data_cleaning_eda.py
@@ -8,3 +8,9 @@ def clean_cat_data(df, cols):
         result_df[col_str] = result_df[col_str].str.replace('.', '')
     result_df = result_df.drop(cols, axis=1)
     return result_df
+
+def get_count_by_df(df, count_col, groupby_col):
+    result_df = df.copy()
+    result_df = result_df[[count_col, groupby_col]].drop_duplicates()
+    result_df = result_df.groupby(groupby_col).count()
+    return result_df
